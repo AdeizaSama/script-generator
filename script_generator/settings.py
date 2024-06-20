5bf76d4735249a47d32125b1778cfe7fac940a69
AdeizaSama <sodiumbadams@yahoo.co.uk>
2024-06-20 16:16:47 +0100

    vercel deployment fix with style

--- script_generator/settings.py
+++ script_generator/settings.py
@@ -25,8 +25,12 @@ SECRET_KEY = 'django-insecure-s3$d7x*ay+#05j^@rmu)a-!lun&zaq!dddwr*7&p$!2!!%bo0*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['script-generator.vercel.app', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = [
+    'script-generator.vercel.app',
+    'script-generator-7mh645bj2-adeizasamas-projects.vercel.app',
+    'localhost',
+    '127.0.0.1'
+]
 
 # Application definition
 
@@ -145,10 +149,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Additional static files settings
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'script_app/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
